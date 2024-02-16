7b2f9d2df18fd938e9bb004b7df6d9b7d0017382
Victor Hugo <victorzarbesu@gmail.com>
2024-02-16 10:49:05 -0300

    Instanciado a classe para consultar o CEP desejado

--- Principal.java
+++ Principal.java
@@ -1,5 +1,8 @@
 public class Principal {
     public static void main(String[] args) {
 
+        ConsultaCep consultaCep = new ConsultaCep();
+        consultaCep.buscaEndereco("03264-070");
+
     }
 }
